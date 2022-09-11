a753a8f2e0d9cdfcb7e211497f7baa40b53662cf
uribo <ur1b0.dev@gmail.com>
2022-09-11 11:18:44 +0000

    authorized_keysからポートとホストの抜き出しを追加

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import pprint
 
 try:
   with open(os.environ['HOME']+"/.ssh/authorized_keys", "r") as f:
@@ -7,3 +8,11 @@ try:
 except Exception as e:
   print(e)
   exit(0)
+
+# #permitlisten=127.0.0.1:{port} {host} を抜き出す
+comment_list = [i[:-1] for i in auth_key_list if i[:2] == "#p"]
+
+# port:hostの辞書を作る
+port_host_dic = {i.split(" ")[0].split(":")[1]:i.split(" ")[1] for i in comment_list if 1 < len(i.split(" "))}
+
+pprint.pprint(port_host_dic)
