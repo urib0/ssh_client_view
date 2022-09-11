6243dc30909fc8187648be5d8019c206743b6d25
uribo <ur1b0.dev@gmail.com>
2022-09-11 12:20:06 +0000

    listenしているポートのリストを作成

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import pprint
+import subprocess
 
 try:
   with open(os.environ['HOME']+"/.ssh/authorized_keys", "r") as f:
@@ -16,3 +17,7 @@ comment_list = [i[:-1] for i in auth_key_list if i[:2] == "#p"]
 port_host_dic = {i.split(" ")[0].split(":")[1]:i.split(" ")[1] for i in comment_list if 1 < len(i.split(" "))}
 
 pprint.pprint(port_host_dic)
+
+cmd = 'ss -altn|grep -oE [0-9]{5}'
+port_list = subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True).communicate()[0].decode("utf-8").split("\n")[:-1]
+print(port_list)
